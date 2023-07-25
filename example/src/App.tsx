be931e139ddc6ead87ef97b0d21bcde8c73b7cc3
Wadah Esam <wadah-wleed@hotmail.com>
2023-07-25 18:55:15 +0300

    feat: add v1 with example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,27 +1,147 @@
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-reanimated-animated-accordion';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from 'react-native';
+import Expandable from 'react-native-reanimated-animated-accordion';
+
+const testImage = require('../assets/icon.png');
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const [expanded, setExpanded] = React.useState(false);
+  const [speed, setSpeed] = React.useState(300);
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View
+        style={{
+          marginBottom: 20,
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
+      >
+        <Text>Speed</Text>
+        <Text style={{ marginStart: 10, fontWeight: '600' }}>{speed}</Text>
+        <TouchableOpacity
+          onPress={() => {
+            setSpeed(speed + 100);
+          }}
+          style={{
+            marginStart: 10,
+            width: 30,
+            height: 30,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'pink',
+            borderRadius: 400,
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: '700',
+            }}
+          >
+            +
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            if (speed <= 0) {
+              return setSpeed(0);
+            }
+            setSpeed(speed - 100);
+          }}
+          style={{
+            marginStart: 10,
+            width: 30,
+            height: 30,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'pink',
+            borderRadius: 400,
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: '700',
+            }}
+          >
+            -
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{
+          width: '80%',
+          borderRadius: 20,
+          backgroundColor: 'white',
+          shadowOffset: {
+            height: -2,
+            width: 0,
+          },
+          elevation: 2,
+          shadowRadius: 20,
+          shadowOpacity: 0.07,
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            padding: 20,
+          }}
+          onPress={() => {
+            setExpanded(!expanded);
+          }}
+        >
+          <Text>Toggle</Text>
+        </TouchableOpacity>
+        <View style={{ width: '100%' }}>
+          <Expandable
+            duration={speed}
+            renderWhenCollapsed={false}
+            expanded={expanded}
+          >
+            <View style={{ width: '100%', padding: 20, paddingTop: 0 }}>
+              <Text>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Necessitatibus ab placeat alias commodi voluptatibus possimus
+                ducimus sit repellat praesentium fugit similique aut quam nemo
+                libero, aperiam deleniti modi natus quia!
+              </Text>
+              <Text>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Necessitatibus ab placeat alias commodi voluptatibus possimus
+                ducimus sit repellat praesentium fugit similique aut quam nemo
+                libero, aperiam deleniti modi natus quia!
+              </Text>
+
+              <View
+                style={{ marginTop: 20, width: '100%', alignItems: 'center' }}
+              >
+                <Image
+                  source={testImage}
+                  style={{ height: 100, width: 100, resizeMode: 'contain' }}
+                />
+              </View>
+            </View>
+          </Expandable>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 100,
     alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
     width: 60,
---
+++ src/Expandable.tsx
@@ -0,0 +1,151 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-native/no-inline-styles */
+import React, { LegacyRef, useEffect } from 'react';
+import { EasingFunction, View } from 'react-native';
+import Animated, {
+  runOnJS,
+  useAnimatedReaction,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from 'react-native-reanimated';
+
+const DEFAULT_DURATION = 400;
+
+const Expandable = ({
+  expanded = false,
+  duration = DEFAULT_DURATION,
+  easing,
+  renderWhenCollapsed = true,
+  children = <></>,
+}: {
+  expanded?: boolean;
+  duration?: number;
+  renderWhenCollapsed?: boolean;
+  easing?: EasingFunction | undefined;
+  children: JSX.Element | JSX.Element[];
+}) => {
+  const [localIsExpanded, setLocalIsExpanded] = React.useState(false);
+  const [isCollapsing, setIsCollapsing] = React.useState(false);
+  const delay = renderWhenCollapsed ? 100 : 0;
+
+  const heightSV = useSharedValue(0);
+  const opacitySV = useSharedValue(0);
+  const innerComponentHeightSV = useSharedValue(0);
+  const innerComponentRef: LegacyRef<View> = useAnimatedRef();
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      height: heightSV.value,
+      opacity: opacitySV.value,
+    };
+  });
+
+  useEffect(() => {
+    if (expanded !== localIsExpanded) {
+      if (expanded) {
+        expand({ _duration: duration });
+      } else {
+        collapse({ _duration: duration });
+      }
+    }
+  }, [expanded]);
+
+  const expand = ({ _duration = DEFAULT_DURATION }) => {
+    'worklet';
+
+    runOnJS(setLocalIsExpanded)(true);
+    opacitySV.value = withDelay(delay, withTiming(1, { duration }));
+    heightSV.value = withDelay(
+      delay,
+      withTiming(
+        innerComponentHeightSV.value,
+        { duration: _duration, ...(easing ? { easing } : {}) },
+        (finished) => {
+          if (finished) {
+            runOnJS(setLocalIsExpanded)(true);
+          }
+        }
+      )
+    );
+  };
+
+  const collapse = ({ _duration = DEFAULT_DURATION }) => {
+    runOnJS(setLocalIsExpanded)(false);
+    runOnJS(setIsCollapsing)(true);
+    opacitySV.value = withDelay(delay, withTiming(0, { duration }));
+    heightSV.value = withDelay(
+      delay,
+      withTiming(0, { duration: _duration }, (finished) => {
+        if (finished) {
+          runOnJS(setLocalIsExpanded)(false);
+          runOnJS(setIsCollapsing)(false);
+        }
+      })
+    );
+  };
+
+  const shouldRenderElement =
+    renderWhenCollapsed || isCollapsing || localIsExpanded;
+
+  useAnimatedReaction(
+    () => {
+      return {
+        innerHeight: innerComponentHeightSV.value,
+        expanded,
+        localIsExpanded,
+      };
+    },
+    (current, previous) => {
+      // check when the inner component is rendered
+      if (
+        previous?.innerHeight === 0 &&
+        current.innerHeight !== 0 &&
+        expanded
+      ) {
+        expand({ _duration: duration });
+      }
+      if (
+        current.innerHeight !== previous?.innerHeight &&
+        current.localIsExpanded === previous?.localIsExpanded
+      ) {
+        if (current.localIsExpanded) {
+          expand({ _duration: duration });
+        }
+      }
+    },
+    [innerComponentHeightSV, localIsExpanded, expanded]
+  );
+
+  return (
+    <Animated.View
+      style={[
+        {
+          overflow: 'hidden',
+          flexDirection: 'column',
+        },
+        animatedStyle,
+      ]}
+    >
+      <View
+        ref={innerComponentRef}
+        style={{ width: '100%', position: 'absolute' }}
+      >
+        <View
+          onLayout={(e) => {
+            const { height } = e.nativeEvent.layout;
+            if (height !== 0 && height !== innerComponentHeightSV.value) {
+              innerComponentHeightSV.value = e.nativeEvent.layout.height;
+            }
+          }}
+        >
+          {shouldRenderElement ? children : <></>}
+        </View>
+      </View>
+    </Animated.View>
+  );
+};
+
+export default Expandable;
